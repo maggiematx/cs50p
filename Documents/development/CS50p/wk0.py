f573fc990313370c81a6fccf11ead2ebf2da049c
Maggie Ma <maggiema@Maggies-MBP.attlocal.net>
2024-05-04 16:33:19 -0500

    note

--- Documents/development/CS50p/wk0.py
+++ Documents/development/CS50p/wk0.py
@@ -7,7 +7,20 @@ name = name.strip()
 # Capitalize user's name
 name=name.title()
 
-
 print(f'Hello,  {name}')
 
 name=input("What's your name?").strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
+print(f"hello, {first}")
+
+
+x=int(input("what's x?"))
+y=int(input("what's y?"))
+print (x+y)
+
+
+x=float(input("what's x?"))
+y=float(input("what's y?"))
+print(round(x+y))
