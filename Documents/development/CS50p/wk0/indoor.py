b9645ee953b9f27cad91f0a69aebc355e556f8a6
Maggie Ma <maggiema@192.168.0.101>
2024-05-16 09:53:16 +0800

    solution

---
+++ Documents/development/CS50p/wk0/indoor.py
@@ -0,0 +1,12 @@
+def main():
+    # Prompt the user for input
+    user_input = input("Please enter a sentence: ")
+
+    # Convert the input to lowercase
+    lowercase_input = user_input.lower()
+
+    # Output the lowercase input
+    print(lowercase_input)
+    
+if __name__ == "__main__":
+    main()
